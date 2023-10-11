513506b995fc22bb560d2cf8513cfc69cb18ae64
Yuri Bohnenberger <yuribohnenberger@gmail.com>
2023-10-10 22:40:18 -0300

    conteúdo abordado em aula

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+
+        final double VALOR_DE_PI = 3.14; //com a palavra reservada "final" eu declaro uma constante -> por convenção, a constante precisa ser declarada toda em caixa alta
+
     }
 }
